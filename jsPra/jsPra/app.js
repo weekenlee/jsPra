37f1a1aaa65d1e6169e13f12684b32506bef02e7
lwj1396@163.com <lwj1396@163.com>
2018-03-14 19:26:13 +0800

    字符串与数字

--- jsPra/jsPra/app.js
+++ jsPra/jsPra/app.js
@@ -15,3 +15,18 @@ function changeColor() {
 changeColor()
 
 console.log(color)
+
+var s1= new String("sometext")
+s1.log="log str"
+console.log(s1.log)
+
+var s2= "sometext"
+//s2.log="log str"  ±¨´í
+console.log(s2.log)
+
+var num  = 99
+console.log(num.toFixed(2))
+console.log(num.toExponential(1))
+console.log(num.toPrecision(1))
+console.log(num.toPrecision(2))
+console.log(num.toPrecision(3))
