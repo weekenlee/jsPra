077b613df1e836f8b02cd0c54319fc650c4c562a
lwj1396 <lwj1396@163.com>
2018-08-31 14:35:17 +0800

    process.argv

--- nodepra/file_watcher.js
+++ nodepra/file_watcher.js
@@ -1,3 +1,7 @@
 const fs = require('fs')
-fs.watch('const.js', ()=>console.log('File Change'))
-console.log("watching const.js")
+var filename = process.argv[2]
+if (filename == undefined || filename == ""){
+    filename = 'const.js'
+}
+fs.watch(filename, () => console.log('File Change'));
+console.log("watching " + filename);
