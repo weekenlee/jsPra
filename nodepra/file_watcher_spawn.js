7485ca99ee9e8201c56bf196114642255579a8c6
lwj1396 <lwj1396@163.com>
2018-08-31 15:21:28 +0800

    spawn

--- nodepra/file_watcher_spawn.js
+++ nodepra/file_watcher_spawn.js
@@ -8,7 +8,13 @@ if (filename == undefined || filename == ""){
 fs.watch(filename, () => {
     console.log('File Change')
     const ls = spawn('ls', ['-l','-h',filename]);
-    ls.stdout.pipe(process.stdout);
+    //ls.stdout.pipe(process.stdout);
+    output='';
+    ls.stdout.on('data', chunk => output += chunk);
+    ls.on('close', ()=>{
+        const parts = output.split(/\s+/)
+        console.log([parts[0], parts[4], parts[8]])
+    })
 });
 
 console.log("watching " + filename);
